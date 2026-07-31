7102000bba6e959b9f65f04a715e4547b5bed69f
argb32 <usrext-alejandrorg@gmrcanarias.com>
2026-07-31 11:54:26 +0100

    add character form

--- src/app/components/pages/dragonball-page/dragonball-page.ts
+++ src/app/components/pages/dragonball-page/dragonball-page.ts
@@ -1,9 +1,57 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+
+}
+
 
 @Component({
   selector: 'app-dragonball-page',
   imports: [],
   templateUrl: './dragonball-page.html'
 })
-export class DragonballPage { }
+export class DragonballPage {
+
+  name = signal('');
+  power = signal(100);
+
+
+
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8001 },
+    { id: 3, name: 'Yamcha', power: 500 },
+    { id: 4, name: 'Picolo', power: 3001 },
+
+  ]);
+
+  // powerClasses = computed(() => {
+  //   return {
+  //     'text-danger': true
+  //   }
+  // })
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    }
+    this.characters.update(list => [...list, newCharacter]);
+    this.resetFields()
+  }
+  resetFields() {
+    this.name.set('');
+    this.power.set(0)
+  }
+
+}
